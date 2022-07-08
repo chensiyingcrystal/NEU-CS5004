87cfb0f0807a3c03815d126b9b967b99ad9debdc
HaozheZhang0818 <zhang.haozhe1@northeastern.edu>
2022-07-08 15:07:55 -0700

    add main to test

---
+++ Lab2/Conservatory/src/birds/Main.java
@@ -0,0 +1,9 @@
+package birds;
+
+public class Main {
+    public static void main(String[] args) {
+        Birds hawk1 = new Hawks("hawk01");
+
+        System.out.println(hawk1.getID());
+    }
+}
